4c03c314d3e5a791668718d9ddf29e4755b26062
Megan Zhao <meganzhao@meganzhao-macbookpro.roam.corp.google.com>
2019-11-20 11:59:04 -0600

    Link reservation usage section to real-time data

--- examples/bigquery-org-wide-running-jobs/static/js/index.js
+++ examples/bigquery-org-wide-running-jobs/static/js/index.js
@@ -1,18 +1,37 @@
+google.charts.load('current', { 'packages': ['treemap'] });
+// 
+
+google.charts.load('current', { 'packages': ['corechart'] });
+google.charts.setOnLoadCallback(drawChartLine);
+
 $(document).ready(function () {
-	$('#job-table').DataTable( {
+	$.ajax({ 
+		type: 'GET', 
+		url: 'https://festive-terrain-1.appspot.com/_ah/get-handlers/v1/jobs', 
+		data: { get_param: 'value' }, 
+		dataType: 'json',
+		success: function (data) { 
+			google.charts.setOnLoadCallback(drawChart(data));
+		}
+	});
+
+
+	$('#job-table').DataTable({
+		// TODO: get data from variable
 		"ajax": {
 			"url": "https://festive-terrain-1.appspot.com/_ah/get-handlers/v1/jobs",
 		},
-        "columns": [
-			{ "data": "jobid",
-              'createdCell':  function (td, cellData, rowData, row, col) {
-				  	$(td).html('<a>' + cellData + '</a>');
+		"columns": [
+			{
+				"data": "jobid",
+				'createdCell': function (td, cellData, rowData, row, col) {
+					$(td).html('<a>' + cellData + '</a>');
 					$(td).click(
-					  function() {
-						  queryJobInfo(rowData);
-					  }
-				  ); 
-                }
+						function () {
+							queryJobInfo(rowData);
+						}
+					);
+				}
 			},
 			{ "data": "email" },
 			{ "data": "projectid" },
@@ -20,14 +39,14 @@ $(document).ready(function () {
 			{ "data": "slots" },
 			{ "data": "state" },
 		]
-	} );
-	$( "#modal-close" ).click(function() {
+	});
+	$("#modal-close").click(function () {
 		$(".modal").removeClass("is-active");
 	});
-	$( "#modal-close1" ).click(function() {
+	$("#modal-close1").click(function () {
 		$(".modal").removeClass("is-active");
 	});
-} );
+});
 
 function queryJobInfo(rowData) {
 	$(".modal").addClass("is-active");
@@ -37,25 +56,25 @@ function queryJobInfo(rowData) {
 var isLive = false;
 var interval;
 var d1h = false;
-$( "#livebutton" ).click(function() {
+$("#livebutton").click(function () {
 	if (!isLive) {
 		$("#livestatus").addClass("has-text-danger");
 		isLive = true;
-		interval = setInterval(function() {
+		interval = setInterval(function () {
 			var today = new Date();
-			var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()+ ' ' + today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+			var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate() + ' ' + today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
 			//$("#currenttime").textText=date;
-			document.getElementById('currenttime').innerHTML = date; 
+			document.getElementById('currenttime').innerHTML = date;
 			//$("#currenttime").load(date);
 		}, 1000);
 	} else {
 		$("#livestatus").removeClass("has-text-danger");
 		isLive = false;
-		clearInterval(interval); 
+		clearInterval(interval);
 	}
 });
-$( "#d1h" ).click(function() {
-	
+$("#d1h").click(function () {
+
 	if (!d1h) {
 		$("#d1h").addClass("has-background-info has-text-white");
 		d1h = true;
@@ -65,43 +84,56 @@ $( "#d1h" ).click(function() {
 	}
 });
 
-google.charts.load('current', {'packages':['treemap']});
-google.charts.setOnLoadCallback(drawChart);
+function sum(arr, key) {
+	var total = 0;
+	for (var i = 0; i < arr.length; i++) {
+		row = arr[i];
+		total += row[key];
+	}
+	return total;
+}
 
-function drawChart() {
-	var data = google.visualization.arrayToDataTable([
-		['Location', 'Parent', 'Market trade volume (size)', 'Market increase/decrease (color)'],
-		['pac/thd/',   		 null,                 0,                               0],
-		['pac/thd/pr',   	'pac/thd/',          0,                               0],
-		['pac/thd/io',    	'pac/thd/',             0,                               0],
-		['pac/thd/npr',     'pac/thd/',      0,                               0],
-		['pac/thd/sc', 		'pac/thd/',             0,                               0],
-		['pac/thd/st',    	'pac/thd/',             0,                               0],
-		['pac/thd/us',   	'pac/thd/pr',           11,                              10],
-		['pac/thd/cn',      'pac/thd/pr',           52,                              31],
-		['pac/thd/eu',    	'pac/thd/pr',           24,                              12],
-		['pac/thd/pr/sc',   'pac/thd/pr',           16,                             -23],
-		['pac/thd/eu/adhoc','pac/thd/eu',             42,                              -11],
-		['pac/thd/eu/sla',  'pac/thd/eu',             31,                              -2],
-		['pac/thd/eu/nosla','pac/thd/eu',             22,                              -13],
-		['pac/thd/eu/dev',  'pac/thd/eu',             17,                              4],
-		['pac/thd/eu/test', 'pac/thd/eu',             21,                              -5],
-		['pac/thd/npr/sla', 'pac/thd/npr',               36,                              4],
-		['pac/thd/npr/nosla','pac/thd/npr',               20,                              -12],
-		['pac/thd/npr/prod','pac/thd/npr',               40,                              63],
-		['pac/thd/npr/dev', 'pac/thd/npr',               4,                               34],
-		['pac/thd/npr/test','pac/thd/npr',               1,                               -5],
-		['pac/thd/npr/uat', 'pac/thd/npr',               12,                              24],
-		['pac/thd/npr/io1', 'pac/thd/npr',               18,                              13],
-		['Pakistan',  		'pac/thd/npr',               11,                              -52],
-		['pac/thd/sc/npa',  'pac/thd/sc',             21,                              0],
-		['pac/thd/sla', 	'pac/thd/sc',             30,                              43],
-		['pac/thd/nosla',   'pac/thd/sc',             12,                              2],
-		['pac/thd/dev',     'pac/thd/sc',             10,                              12],
-		['pac/thd/test',    'pac/thd/sc',             8,                               10]
-		]);
+function groupBy(xs, key) {
+	return xs.reduce(function (rv, x) {
+		(rv[x[key]] = rv[x[key]] || []).push(x);
+		return rv;
+	}, {});
+}
 
-	
+function reservationUsage(jsonData) {
+	// initialization
+	// create a json obj to store slots?
+	var arr = [];
+	arr.push(['Location', 'Parent', 'Market trade volume (size)', 'Market increase/decrease (color)']);
+	arr.push(["all", null, 0, 0]);
+
+	console.log(jsonData);
+
+	// group by reservation id
+	var groupbyReservationId = groupBy(jsonData["data"], "reservationid");
+	console.log(groupbyReservationId);
+	for (var reservationId in groupbyReservationId) {
+		var slotsbyReservation = sum(groupbyReservationId[reservationId], "slots")
+		arr.push([reservationId, "all", slotsbyReservation, 0]);
+
+		var slotsbyProject = slotsbyReservation / groupbyReservationId[reservationId].length;
+		var groupbyProject = groupBy(groupbyReservationId[reservationId], 'projectid');
+		for (var projectId in groupbyProject) {
+			arr.push([projectId, reservationId, slotsbyProject, 0]);
+
+			var slotsbyUser = slotsbyProject / groupbyProject[projectId].length;
+			var groupbyUser = groupBy(groupbyProject[projectId], 'email');
+			for (var email in groupbyUser) {
+				arr.push([email, projectId, slotsbyUser, 0]);
+			}
+		}
+		console.log(arr);
+	}
+	return arr;
+}
+
+function drawChart(jsonData) {
+	var data = google.visualization.arrayToDataTable(reservationUsage(jsonData));
 	tree = new google.visualization.TreeMap(document.getElementById('chart_div'));
 	tree.draw(data, {
 		minColor: '#007000',
@@ -109,10 +141,10 @@ function drawChart() {
 		maxColor: '#D2222D',
 		headerHeight: 25,
 		fontColor: 'black'
-		,showScale: true
+		, showScale: true
 	});
 
-	google.visualization.events.addListener(tree, 'select', function(){
+	google.visualization.events.addListener(tree, 'select', function () {
 		var selectedItem = tree.getSelection()[0];
 		var size = 10;
 		var value = 20;
@@ -133,22 +165,19 @@ function drawChart() {
 	': ' + value );
 	*/
 		}
-		
+
 	});
 }
-
-google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawChartLine);
 
 function drawChartLine(activeunits, completedunits, pendingunits, elapsed) {
 	// TODO: cut all arrays to last 25
 
 	var dataArray = [['elapsed', 'activeunits', 'completedunits', 'pendingunits']];
 
-	if (activeunits === undefined || completedunits === undefined || 
+	if (activeunits === undefined || completedunits === undefined ||
 		pendingunits === undefined || elapsed === undefined) {
 		return;
-	} 
+	}
 
 	for (var n = 0; n < activeunits.length; n++) {
 		dataArray.push([elapsed[n], activeunits[n], completedunits[n], pendingunits[n]]);
@@ -162,21 +191,21 @@ function drawChartLine(activeunits, completedunits, pendingunits, elapsed) {
 	// 	['10:01:01',  4000,      4000, 100],
 	// 	['10:01:02',  3900,      4000, 120],
 	// 	]);
-	
+
 	var options = {
 		pointSize: 2,
 		curveType: 'function',
 		legend: 'top',
 		height: 600,
 		width: 600,
-		chartArea: {'width': '70%', 'height': '70%'},
+		chartArea: { 'width': '70%', 'height': '70%' },
 		// Gives each series an axis that matches the vAxes number below.
 		series: {
-			0: {targetAxisIndex: 0},
-			2: {targetAxisIndex: 2}
+			0: { targetAxisIndex: 0 },
+			2: { targetAxisIndex: 2 }
 		},
 		hAxis: {
-			title: 'Timeline', 
+			title: 'Timeline',
 			slantedText: true,
 			gridlines: {
 				count: 11
@@ -185,13 +214,11 @@ function drawChartLine(activeunits, completedunits, pendingunits, elapsed) {
 		vAxis: {
 			// Adds titles to each axis.
 			0: {
-			title: 'Work Units'
+				title: 'Work Units'
 			},
 			2: {
-			title: 'Slots utilized'
+				title: 'Slots utilized'
 			}
-			
-			
 		}
 	};
 
